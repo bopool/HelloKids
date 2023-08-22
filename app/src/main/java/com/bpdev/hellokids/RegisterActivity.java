335f944cccce05512a002111536c0a9a274d3be6
bopool <mia-uu@hanmail.net>
2023-08-22 17:40:02 +0900

    회원가입 양식 디자인 변경

--- app/src/main/java/com/bpdev/hellokids/RegisterActivity.java
+++ app/src/main/java/com/bpdev/hellokids/RegisterActivity.java
@@ -1,10 +1,19 @@
 package com.bpdev.hellokids;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText userName;
+    EditText userId;
+    EditText userPassword;
+    EditText userPassword2;
+    EditText userEmail;
+    EditText userPhoneNumber;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
