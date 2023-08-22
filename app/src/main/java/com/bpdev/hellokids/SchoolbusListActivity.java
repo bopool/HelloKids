aafa73274207af95ab1654348a27c60007e8d819
bopool <mia-uu@hanmail.net>
2023-08-22 15:29:16 +0900

    activity_register_select 추가

---
+++ app/src/main/java/com/bpdev/hellokids/SchoolbusListActivity.java
@@ -0,0 +1,14 @@
+package com.bpdev.hellokids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SchoolbusListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schoolbus_list);
+    }
+}
