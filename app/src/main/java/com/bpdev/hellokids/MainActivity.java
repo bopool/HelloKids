fbc6a43f42bc354ec87fd3aab424b197fdcce9df
HYEEYH <hye890@daum.net>
2023-08-24 11:19:46 +0900

    사진첩-자세히보기화면 완성

--- app/src/main/java/com/bpdev/hellokids/MainActivity.java
+++ app/src/main/java/com/bpdev/hellokids/MainActivity.java
@@ -14,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
 
     Button button10;
 
+    // 혜리 - 테스트용 페이지 연결 : 테스트하려고 만든 액티비티 연결 버튼
+    TextView textPageTitle;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // 사진첩 테스트용 버튼 - 나중에 지우기
+        textPageTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AlbumViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
