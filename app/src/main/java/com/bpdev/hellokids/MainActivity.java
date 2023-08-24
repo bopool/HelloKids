7a5b85d0fa4de800108faf4aa7890e2363f7e3c5
YeojinSon7 <130967465+YeojinSon7@users.noreply.github.com>
2023-08-24 09:37:13 +0900

    안심등하원 차량정보 등록 화면 완성

--- app/src/main/java/com/bpdev/hellokids/MainActivity.java
+++ app/src/main/java/com/bpdev/hellokids/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView btnSignup;
+
+    Button button10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnSignup = findViewById(R.id.btnSignup);
+        button10 = findViewById(R.id.button10);
 
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,RegisterSelectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SchoolbusAddActivity.class);
                 startActivity(intent);
             }
         });
