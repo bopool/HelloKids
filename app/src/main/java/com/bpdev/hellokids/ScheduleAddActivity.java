6fd6a03b6dbf0e282e361422021c283a1213b975
HYEEYH <hye890@daum.net>
2023-08-28 12:07:55 +0900

    일정표 - 리스트,달력,추가,수정페이지 완성

---
+++ app/src/main/java/com/bpdev/hellokids/ScheduleAddActivity.java
@@ -0,0 +1,14 @@
+package com.bpdev.hellokids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScheduleAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_add);
+    }
+}
