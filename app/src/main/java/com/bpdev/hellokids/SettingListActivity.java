ad8f23b89699cfd544b83c1837909876f25b9fde
HYEEYH <hye890@daum.net>
2023-08-28 14:48:22 +0900

    버튼아이디통일-메인,회원가입,로그인화면 완성

--- app/src/main/java/com/bpdev/hellokids/SettingListActivity.java
+++ app/src/main/java/com/bpdev/hellokids/SettingListActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SettinglistActivity extends AppCompatActivity {
+public class SettingListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settinglist);
+        setContentView(R.layout.activity_setting_list);
     }
 }
