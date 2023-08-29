2ab0b2ee40d9a37930f614609f36fd97d1655a9c
HYEEYH <hye890@daum.net>
2023-08-29 17:15:48 +0900

    설정-차량등록수정삭제 화면 추가 및 아이디 통일

---
+++ app/src/main/java/com/bpdev/hellokids/SettingSchoolbusEditActivity.java
@@ -0,0 +1,14 @@
+package com.bpdev.hellokids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingSchoolbusEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_schoolbus_edit);
+    }
+}
