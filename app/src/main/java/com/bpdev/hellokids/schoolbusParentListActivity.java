bdd0bdcd3488f7ba428e4090c0ed4b04c548dec8
HYEEYH <hye890@daum.net>
2023-08-29 13:22:36 +0900

    안심등하원(선생님)인포화면까지 아이디와 화면 세팅

--- app/src/main/java/com/bpdev/hellokids/schoolbusParentListActivity.java
+++ app/src/main/java/com/bpdev/hellokids/schoolbusParentListActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SchoolBusInfoActivity extends AppCompatActivity {
+public class schoolbusParentListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_school_bus_info);
+        setContentView(R.layout.activity_schoolbus_parent_list);
     }
 }
