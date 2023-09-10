e1be519d04b370cd7af0bedc38bbabd3a4ddb78f
HYEEYH <130967557+HYEEYH@users.noreply.github.com>
2023-09-11 00:08:10 +0900

    사진첩 얼굴인식액티비티 추가

---
+++ app/src/main/java/com/bpdev/hellokids/PhotoalbumAddRekogActivity.java
@@ -0,0 +1,406 @@
+package com.bpdev.hellokids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bpdev.hellokids.adapter.ScheduleAdapter;
+import com.bpdev.hellokids.api.NetworkClient;
+import com.bpdev.hellokids.api.SettingApi;
+import com.bpdev.hellokids.config.Config;
+import com.bpdev.hellokids.model.ClassList;
+import com.bpdev.hellokids.model.NurseryClass;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class PhotoalbumAddRekogActivity extends AppCompatActivity {
+
+    // 최상단 헤더 버튼
+    TextView btnRegister;
+    TextView btnLogin;
+    ImageButton btnTranslate;
+
+
+    // 하단 바로가기 메뉴 버튼
+    Button btnBottomHome;
+    Button btnBottomNotice;
+    Button btnBottomDailyNote;
+    Button btnBottomSchoolbus;
+    Button btnBottomSetting;
+
+    // 메인 파트 버튼
+    Button btnAdd;
+    Button btnSelectDate;
+
+    // 작성일 선택
+    DatePickerDialog datePickerDialog;
+    String date1;
+
+
+    // 스피너, 반 이름
+    Spinner spinnerClass;
+    ScheduleAdapter adapter;
+    ArrayList<String> classNameArrayList = new ArrayList<>(); // 스피너에 넣어줄 반 목록
+    ArrayList<NurseryClass> classArrayList = new ArrayList<>(); // api에 쓸 것
+    ArrayAdapter<String> arrayAdapter;
+    HashMap<String, Integer> map = new HashMap<>(); // 스피너에 들어가있는 반이름을 클릭하면 그 반이름을 가진 반 데이터의 id를 반환할 때 사용
+    int classId1;
+
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photoalbum_add_rekog);
+
+
+        // 최상단 헤더 버튼 화면 연결
+        btnRegister = findViewById(R.id.btnRegister);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnTranslate = findViewById(R.id.btnTranslate);
+
+        // 하단 바로가기 메뉴 화면 연결
+        btnBottomHome = findViewById(R.id.btnBottomHome);
+        btnBottomNotice = findViewById(R.id.btnBottomNotice);
+        btnBottomDailyNote = findViewById(R.id.btnBottomDailynote);
+        btnBottomSchoolbus = findViewById(R.id.btnBottomSchoolbus);
+        btnBottomSetting = findViewById(R.id.btnBottomSetting);
+
+        // 메인 파트 화면 연결
+
+
+
+        // 스피너 연결
+        spinnerClass = findViewById(R.id.spinnerClass);
+
+
+        // -- -- -- 메인 파트 동작 -- -- -- //
+        // 스피너
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, classNameArrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // 스피너에 반 이름 가져오기
+        Retrofit retrofit = NetworkClient.getRetrofitClient(PhotoalbumAddRekogActivity.this);
+        SettingApi api = retrofit.create(SettingApi.class);
+
+        SharedPreferences sp = getSharedPreferences(Config.PREFERENCE_NAME, MODE_PRIVATE);
+        String token = sp.getString(Config.ACCESS_TOKEN, "");
+
+        Call<ClassList> call = api.classListView("Bearer " + token);
+        call.enqueue(new Callback<ClassList>() {
+            @Override
+            public void onResponse(Call<ClassList> call, Response<ClassList> response) {
+                if (response.isSuccessful()) {
+                    ClassList classList = response.body();
+                    classArrayList.addAll(classList.getItems());
+
+                    for (int i = 0; i < classArrayList.size(); i++) {
+                        classNameArrayList.add(classArrayList.get(i).getClassName());
+                        map.put(classArrayList.get(i).getClassName(), classArrayList.get(i).getId());
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+                } else {
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ClassList> call, Throwable t) {
+            }
+        });
+
+        spinnerClass.setAdapter(arrayAdapter);
+
+        spinnerClass.setSelection(0);
+
+
+
+        spinnerClass.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String spinnerValue = adapterView.getItemAtPosition(i).toString();
+                spinnerClass.setSelection(i);
+                Toast.makeText(getApplicationContext(), spinnerValue+"이 선택되었습니다.", Toast.LENGTH_SHORT).show();
+
+                classId1 = map.get(spinnerValue);
+
+                //String.valueOf(classId1);
+
+
+                Log.i("classId", classId1 + "");
+
+
+//                // 반별 일정표 리스트 조회  ------>>> 여기서부터 반 목록 가져오는걸로 바꾸면 된다.
+//                Retrofit retrofit1 = NetworkClient.getRetrofitClient(PhotoalbumAddActivity.this);
+//
+//                ScheduleApi api1 = retrofit1.create(ScheduleApi.class);
+//
+//                SharedPreferences sp1 = getSharedPreferences(Config.PREFERENCE_NAME, MODE_PRIVATE);
+//                String token1 = sp1.getString(Config.ACCESS_TOKEN, "");
+//
+//                Log.i("token1", token1);
+//
+//                Call<ScheduleList> call1 = api1.scheduleClassList(classId, "Bearer " + token1);
+//                call1.enqueue(new Callback<ScheduleList>() {
+//                    @Override
+//                    public void onResponse(Call<ScheduleList> call, Response<ScheduleList> response) {
+//                        if (response.isSuccessful()) {
+//                            ScheduleList scheduleList1 = response.body();
+//
+//                            scheduleArrayList.addAll(scheduleList1.getItems());
+//
+//                            //Adapter를 이용해서 postInfo에 있는 내용을 가져와서 저장해둔 listView 형식에 맞게 띄움
+//                            adapter = new ScheduleAdapter(PhotoalbumAddActivity.this, scheduleArrayList);
+//                            recyclerView.setAdapter(adapter);
+//                            scheduleArrayList = new ArrayList<>(); // 중복 방지 위한 초기화
+//
+//
+//                        } else {
+//
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<ScheduleList> call, Throwable t) {
+//
+//                    }
+//                });
+
+            }
+
+
+
+            public void onNothingSelected(AdapterView<?> adapterView) { // 아무것도 선택하지 않았을 때 실행되는건데 자동으로 선택이 되기때문에 이 코드가 실행되지 않는다
+
+//                Retrofit retrofit2 = NetworkClient.getRetrofitClient(PhotoalbumAddActivity.this);
+//
+//                ScheduleApi api2 = retrofit2.create(ScheduleApi.class);
+//
+//                SharedPreferences sp2 = getSharedPreferences(Config.PREFERENCE_NAME, MODE_PRIVATE);
+//                String token2 = sp2.getString(Config.ACCESS_TOKEN, "");
+//
+//                Log.i("token2", token2);
+//
+//                Call<ScheduleList> call2 = api2.scheduleList("Bearer " + token2);
+//                call2.enqueue(new Callback<ScheduleList>() {
+//                    @Override
+//                    public void onResponse(Call<ScheduleList> call, Response<ScheduleList> response) {
+//                        if (response.isSuccessful()) {
+//                            ScheduleList scheduleList2 = response.body();
+//
+//                            Log.i("aaa2", scheduleList2.getResult());
+//
+//                            scheduleArrayList.addAll(scheduleList2.getItems());
+//
+//                            //Adapter를 이용해서 postInfo에 있는 내용을 가져와서 저장해둔 listView 형식에 맞게 띄움
+//                            adapter = new ScheduleAdapter(PhotoalbumAddActivity.this, scheduleArrayList);
+//
+//                            recyclerView.setAdapter(adapter);
+//                        } else {
+//
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<ScheduleList> call, Throwable t) {
+//
+//                    }
+//                });
+
+            }
+        });
+
+
+        // 등록하기 버튼
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(PhotoalbumAddRekogActivity.this,PhotoalbumListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // 작성일 선택 버튼
+        // - 달력 띄워서 선택 날짜 가져오기
+        btnSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // 달력 띄우기
+                Calendar calendar = Calendar.getInstance();
+                int year1 = calendar.get(Calendar.YEAR);
+                int month1 = calendar.get(Calendar.MONTH);
+                int day1 = calendar.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(PhotoalbumAddRekogActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year1, int month1, int day1) {
+
+                                // 1월부터 시작하는데 시작이 0이므로 +1 해준다
+                                month1 = month1 +1;
+
+                                // 10 이하의 날짜가 03 이런식으로 나오게 표시 방법 바꾸기
+                                String month;
+                                if ( month1 < 10 ){
+                                    month = "0" + month1;
+                                }else{
+                                    month = "" + month1; // 문자열로 만들기
+                                }
+
+                                String day;
+                                if ( day1 < 10 ){
+                                    day = "0" + day1;
+                                }else{
+                                    day = "" + day1; // 문자열로 만들기
+                                }
+
+                                date1 = ""+ year1 + "-" + month + "-" + day;
+
+                                btnSelectDate.setText(date1);
+                            }
+                        },
+                        year1, month1, day1);
+                datePickerDialog.show();
+
+            }
+        });
+
+
+        // 파일 선택하기 버튼
+        btnSelectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Intent intent = new Intent(Intent.ACTION_PICK);
+//                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+//                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+//                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                startActivityForResult(intent, 2222);
+
+                //setResult(1111, intent);
+
+                // launcher.launch(intent);
+
+
+                //함수 호출
+                showDialog();
+            }
+        });
+
+
+
+        // -- -- -- -- -- 공통 버튼 -- -- -- -- -- //
+        // 최상단 헤더 버튼
+        // 회원가입 버튼
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PhotoalbumAddRekogActivity.this,RegisterSelectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // 로그인 버튼
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PhotoalbumAddRekogActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 번역 버튼
+
+
+
+
+        // 하단 바로가기 메뉴 버튼
+        // 홈 바로가기
+        btnBottomHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PhotoalbumAddRekogActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // 공지사항 바로가기
+        btnBottomNotice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PhotoalbumAddRekogActivity.this, NoticeListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // 알림장 바로가기
+        btnBottomDailyNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PhotoalbumAddRekogActivity.this, DailynoteListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // 안심등하원 바로가기
+        btnBottomSchoolbus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // 선생님화면
+                Intent intent = new Intent(PhotoalbumAddRekogActivity.this, SchoolbusListActivity.class);
+                startActivity(intent);
+
+                // 학부모화면
+                // Intent intent = new Intent(MainActivity.this, SchoolbusParentListActivity.class);
+                // startActivity(intent);
+            }
+        });
+
+
+        // 설정 바로가기
+        btnBottomSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PhotoalbumAddRekogActivity.this, SettingListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // -- -- -- -- -- 공통 버튼  끝 -- -- -- -- -- //
+
+
+
+
+    }
+}
